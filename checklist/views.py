aa0e7d53cb977510a3a661dd53c18ae27d096f5e
John Goewert <john@goewert.org>
2022-01-19 12:59:11 -0500

    Initial Commit

---
+++ checklist/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+
+#from .models import Greeting
+import requests
+import json
+
+class Piece:
+    def __init__(self, num, color, img, qty):
+        self.num = num
+        self.color = color
+        self.img = img
+        self.qty = qty
+
+def sort_by_key(list):
+    return list["part"]["name"]
+
+# Create your views here.
+def index(request, set_id="1682-1"):
+    set_id = request.GET.get('set_id','1682-1')
+    fp = requests.get('https://rebrickable.com/api/v3/lego/sets/' + set_id + '?key=' + settings.REBRICKABLE_API_KEY)
+    decoded = json.loads(fp.text)
+    fp.close()
+
+    set_name = decoded["name"]
+   
+    fp = requests.get('https://rebrickable.com/api/v3/lego/sets/' + set_id + '/parts/?page_size=1000&key=' + settings.REBRICKABLE_API_KEY)
+    decoded = json.loads(fp.text)
+    fp.close()
+    
+    sortedresults = sorted(decoded["results"], key=sort_by_key)
+    set_pieces = []
+
+    for part in sortedresults:
+        if (part['is_spare'] is not True):
+            piece = Piece(part["part"]["part_num"], part["color"]["name"], part["part"]["part_img_url"], int(part["quantity"]))
+            set_pieces.append(piece)
+
+    return render(request, "index.html", {"set_id": set_id, "set_name": set_name, "set_pieces": set_pieces})
