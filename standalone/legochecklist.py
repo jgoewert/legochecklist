61797908064f7c40d7baac47306aa1dc9d5afc0d
John Goewert <john@goewert.org>
2023-01-22 11:15:11 -0500

    Add flowlayout

--- standalone/legochecklist.py
+++ standalone/legochecklist.py
@@ -1,12 +1,16 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QPushButton, QSizePolicy, QScrollArea, QVBoxLayout, QSpinBox
-from PyQt6.QtGui import QPixmap, QIcon
-from PyQt6.QtCore import Qt, QSize
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QPushButton, QSizePolicy, QScrollArea, QVBoxLayout, QSpinBox, QLayout
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtCore import Qt, QSize, QRect, QPoint, QMargins
 import requests
 import json
 import os, os.path
 
 #import urllib.request
+
+part_width = 200
+set_pieces = []
+
 
 class Piece:
     stylesheet = """QSpinBox {
@@ -41,6 +45,31 @@ QSpinBox::up-button  {
         self.color = color
         self.img = img
         self.qty = qty
+        block = QGridLayout()
+        block.addWidget(QLabel(self.num + " - " + self.name),0,0,1,2)
+        partImage = self.getPixmap()
+        partImage.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(partImage,1,0,3,1)
+        quantityLabel = QLabel("Want\n" + str(self.qty))
+        quantityLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(quantityLabel,1,1)
+        haveLabel = QLabel("Have")
+        haveLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(haveLabel,2,1)
+        spinbox = QSpinBox()
+        spinbox.setMaximum(self.qty)
+        spinbox.valueChanged.connect(self.spinboxchanged)
+        spinbox.setStyleSheet(self.stylesheet)
+        spinbox.setMaximumWidth(100)
+        spinbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        block.addWidget(spinbox,3,1)
+        
+        self.widget = QWidget()
+        self.widget.setStyleSheet("background-color: white;")
+        self.widget.setBaseSize(80,part_width)
+        self.widget.setMaximumWidth(part_width)
+        self.widget.setMinimumWidth(part_width)
+        self.widget.setLayout(block)
 
     def downloadImage(self):
         if (self.img is not None):
@@ -76,30 +105,6 @@ QSpinBox::up-button  {
             self.widget.setStyleSheet("background-color: white;")
 
     def getWidget(self):
-        block = QGridLayout()
-        block.addWidget(QLabel(self.num + " - " + self.name),0,0,1,2)
-        partImage = self.getPixmap()
-        partImage.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(partImage,1,0,3,1)
-        quantityLabel = QLabel("Want\n" + str(self.qty))
-        quantityLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(quantityLabel,1,1)
-        haveLabel = QLabel("Have")
-        haveLabel.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(haveLabel,2,1)
-        spinbox = QSpinBox()
-        spinbox.setMaximum(self.qty)
-        spinbox.valueChanged.connect(self.spinboxchanged)
-        spinbox.setStyleSheet(self.stylesheet)
-        spinbox.setMaximumWidth(100)
-        spinbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        block.addWidget(spinbox,3,1)
-        
-        self.widget = QWidget()
-        self.widget.setStyleSheet("background-color: white;")
-        self.widget.setBaseSize(80,200)
-        self.widget.setMaximumWidth(200)
-        self.widget.setLayout(block)
         return self.widget
 
 def sort_by_name(list):
@@ -111,25 +116,113 @@ def sort_by_color(list):
 def sort_by_partnum(list):
     return list["part"]["part_num"]
 
+class FlowLayout(QLayout):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        if parent is not None:
+            self.setContentsMargins(QMargins(0, 0, 0, 0))
+
+        self._item_list = []
+
+    def __del__(self):
+        item = self.takeAt(0)
+        while item:
+            item = self.takeAt(0)
+
+    def addItem(self, item):
+        self._item_list.append(item)
+
+    def count(self):
+        return len(self._item_list)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self._item_list):
+            return self._item_list[index]
+
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self._item_list):
+            return self._item_list.pop(index)
+
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientation(0)
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self._do_layout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self._do_layout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+
+        for item in self._item_list:
+            size = size.expandedTo(item.minimumSize())
+
+        size += QSize(2 * self.contentsMargins().top(), 2 * self.contentsMargins().top())
+        return size
+
+    def _do_layout(self, rect, test_only):
+        x = rect.x()
+        y = rect.y()
+        line_height = 0
+        spacing = self.spacing()
+
+        for item in self._item_list:
+            style = item.widget().style()
+            layout_spacing_x = style.layoutSpacing(
+                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal
+            )
+            layout_spacing_y = style.layoutSpacing(
+                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical
+            )
+            space_x = spacing + layout_spacing_x
+            space_y = spacing + layout_spacing_y
+            next_x = x + item.sizeHint().width() + space_x
+            if next_x - space_x > rect.right() and line_height > 0:
+                x = rect.x()
+                y = y + line_height + space_y
+                next_x = x + item.sizeHint().width() + space_x
+                line_height = 0
+
+            if not test_only:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = next_x
+            line_height = max(line_height, item.sizeHint().height())
+
+        return y + line_height - rect.y()
 
 def main():
     REBRICKABLE_API_KEY = "faef0b1264b7f80e277361182e013d07"
     set_id="1682-1"
     sort_algorithm="name"
     
-
     app = QApplication([])
     window = QWidget()
+    scrollarea = QScrollArea()
+    scrollwidget = QWidget()
+    scrollvbox = FlowLayout()
+
     window.setWindowTitle("Lego Checklist Generator")
     window.setGeometry(100,100,1280,980)
-   
+    #window.setFixedWidth(1280)
     toplayout = QGridLayout()
     toplayout.addWidget((QPushButton("Uncomplete")), 0,0)
     toplayout.addWidget((QPushButton("Complete")), 0,1)
 
-    scrollarea = QScrollArea()
-    scrollwidget = QWidget()
-    scrollvbox = QVBoxLayout()
 
     #set_id = request.GET.get('set_id','1682-1')
     #sort_algorithm = request.GET.get('sort_algorithm','name')
@@ -152,7 +245,6 @@ def main():
         case 'partnum': 
             sortedresults = sorted(decoded["results"], key=sort_by_partnum)
     
-    set_pieces = []
     for part in sortedresults:
         if (part['is_spare'] is not True):
             piece = Piece(part["part"]["part_num"], part["color"]["name"], part["part"]["part_img_url"], int(part["quantity"]), part["part"]["name"])
@@ -160,7 +252,7 @@ def main():
 
     for part in set_pieces:
         scrollvbox.addWidget(part.getWidget())
-
+  
     scrollwidget.setLayout(scrollvbox)
 
     scrollarea.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
@@ -175,8 +267,6 @@ def main():
     mainlayout.addLayout(toplayout,0,0)
     mainlayout.addLayout(scrolllayout,1,0)
     window.setLayout(mainlayout)
-
-
     window.show()
     sys.exit(app.exec())
 
