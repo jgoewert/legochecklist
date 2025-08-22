22eaf4795f8a03d2dc101f100bd3530c855a99d0
John Goewert <john@goewert.org>
2025-08-22 10:51:44 -0400

    Change get set and part functions to handle not finding sets better

--- checklist/views.py
+++ checklist/views.py
@@ -23,29 +23,61 @@ def sort_by_color(list):
 def sort_by_partnum(list):
     return list["part"]["part_num"]
 
+def fetch_set_data(set_id):
+        url = f'https://rebrickable.com/api/v3/lego/sets/{set_id}?key={settings.REBRICKABLE_API_KEY}'
+        fp = requests.get(url)
+        if fp.status_code != 200:
+            # Try with -1 appended to set_id
+            set_id_alt = set_id + '-1' if not set_id.endswith('-1') else set_id
+            url_alt = f'https://rebrickable.com/api/v3/lego/sets/{set_id_alt}?key={settings.REBRICKABLE_API_KEY}'
+            fp = requests.get(url_alt)
+            if fp.status_code != 200:
+                fp.close()
+                return None, None
+            set_id = set_id_alt
+        decoded = json.loads(fp.text)
+        fp.close()
+        return decoded, set_id
+
+def fetch_parts_data(set_id):
+        url = f'https://rebrickable.com/api/v3/lego/sets/{set_id}/parts/?page_size=1000&key={settings.REBRICKABLE_API_KEY}'
+        fp = requests.get(url)
+        if fp.status_code != 200:
+            # Try with -1 appended to set_id
+            set_id_alt = set_id + '-1' if not set_id.endswith('-1') else set_id
+            url_alt = f'https://rebrickable.com/api/v3/lego/sets/{set_id_alt}/parts/?page_size=1000&key={settings.REBRICKABLE_API_KEY}'
+            fp = requests.get(url_alt)
+            if fp.status_code != 200:
+                fp.close()
+                return None
+            set_id = set_id_alt
+        decoded = json.loads(fp.text)
+        fp.close()
+        return decoded
 
 # Create your views here.
 def index(request, set_id="1682-1", sort_algorithm="name"):
-    set_id = request.GET.get('set_id','1682-1')
-    sort_algorithm = request.GET.get('sort_algorithm','name')
-    fp = requests.get('https://rebrickable.com/api/v3/lego/sets/' + set_id + '?key=' + settings.REBRICKABLE_API_KEY)
-    decoded = json.loads(fp.text)
-    fp.close()
+    set_id = request.GET.get('set_id', '1682-1')
+    sort_algorithm = request.GET.get('sort_algorithm', 'name')
+
+    decoded, set_id = fetch_set_data(set_id)
+    if not decoded:
+        return HttpResponse("Set not found.", status=404)
 
     set_name = decoded["name"]
-   
-    fp = requests.get('https://rebrickable.com/api/v3/lego/sets/' + set_id + '/parts/?page_size=1000&key=' + settings.REBRICKABLE_API_KEY)
-    decoded = json.loads(fp.text)
-    fp.close()
-    
+
+    decoded_parts = fetch_parts_data(set_id)
+    if not decoded_parts:
+        return HttpResponse("Set parts not found.", status=404)
+
     match (sort_algorithm):
-        case 'name': 
-            sortedresults = sorted(decoded["results"], key=sort_by_name)
-        case 'color': 
-            sortedresults = sorted(decoded["results"], key=sort_by_color)
-        case 'partnum': 
-            sortedresults = sorted(decoded["results"], key=sort_by_partnum)
-    
+        case 'name':
+            sortedresults = sorted(decoded_parts["results"], key=sort_by_name)
+        case 'color':
+            sortedresults = sorted(decoded_parts["results"], key=sort_by_color)
+        case 'partnum':
+            sortedresults = sorted(decoded_parts["results"], key=sort_by_partnum)
+
     set_pieces = []
     for part in sortedresults:
         if (part['is_spare'] is not True):
